f79848f8f3ae933eceeb14994042083ee01d043f
Sarah Warren <swarrencode@gmail.com>
2023-08-25 12:39:01 +0200

    fix issue with eslintrc include in tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true,
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname
   },
   plugins: ['@typescript-eslint', 'prettier'],

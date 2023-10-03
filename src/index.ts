d3e49c9e0ace8e8a250f8d0adf16438fb6604b04
Sarah <swarrencode@gmail.com>
2023-10-03 11:52:00 +0200

    Update index.ts with Jest testing port settings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 export const runApp = (): string => 'Testing';
+
+// if running a server with Jest testing:
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
